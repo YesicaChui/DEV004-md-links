8720957ab733e83b3543a8ea42499a7271a77afd
YesicaChui <yesicachuic@gmail.com>
2023-03-26 22:00:47 -0500

    leyendo datos con process.argv

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-module.exports = () => {
+/* module.exports = () => {
   // ...
-};
+}; */
+
+import { argv } from 'node:process';
+
+console.log(argv)
+// print process.argv
+argv.forEach((val, index) => {
+  console.log(`${index}: ${val}`);
+});
