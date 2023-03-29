7ead5f08cd9c2637392c776c43f81cd004a09c1d
YesicaChui <yesicachuic@gmail.com>
2023-03-28 21:51:42 -0500

    analizando links de archivo md

--- index.js
+++ index.js
@@ -3,36 +3,47 @@ import { argv } from 'process';
 // https://flaviocopes.com/fix-dirname-not-defined-es-module-scope/
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import fs from 'fs'
+import{ JSDOM } from 'jsdom'
+import {marked} from 'marked'
 
-//console.log(argv)
-// print process.argv
-argv.forEach((val, index) => {
+/* argv.forEach((val, index) => {
   console.log(`${index}: ${val}`);
-});
+}); */
 
 // consiguiendo la ruta absoluta del proyecto
-const __dirname = dirname(fileURLToPath(import.meta.url))
-console.log(__dirname)
+/* const __dirname = dirname(fileURLToPath(import.meta.url))
+console.log(__dirname) */
 
-/* const dividendo= argv[2]
-const divisor = argv[3]
-const aDividir = (dividendo,divisor)=>dividendo/divisor
-console.log(aDividir(dividendo,divisor)) */
 
-/* const dividir = (dividendo, divisor)=>{
-  // al crear una promesa estamos pasando un callback con dos parametros resolve y reject
-  return new Promise((resolve,reject)=>{
-    if(divisor===0){
-      //rechazamos la promesa porque no es posible dividir entre 0, con un mensaje String
-      reject("No se puede dividir entre cero")
-    }else{
-      //si los valores son validos se resuelve la promesa y resolvemos con el resultado de la operacion un Number
-      resolve(dividendo/divisor)
-    }
+//fs.writeFileSync('./ejemplo.txt', 'hola karen!!!')
+const contenido = fs.readFileSync('./test.md','utf-8')
+const textoHtml = marked(contenido)
+console.log(contenido)
+console.log(textoHtml)
+const dom = new JSDOM(textoHtml);
+//console.log(dom.window.document.querySelectorAll("a")); 
+const anclas = dom.window.document.querySelectorAll("a")
+const arregloResultado = []
+for(const elemento of anclas){
+  console.log(elemento.href)
+  arregloResultado.push({
+    href:elemento.href,
+    text:elemento.textContent,
+    file:"./test.md"
   })
 }
-
-// al usar la promesa se utiliza then para el resolve y catch para el reject
-dividir(dividendo,divisor)
-  .then(resultado => console.log(resultado))
-  .catch(error => console.log(error)) */
+console.log(arregloResultado)
+const arregloResultadoFiltrado = arregloResultado.filter((elemento)=>elemento.href.startsWith("http"))
+console.log(arregloResultadoFiltrado)
+/* const markdownText = contenido
+const htmlText = marked(markdownText);
+const dom = new JSDOM(htmlText);
+const links = dom.window.document.querySelectorAll('a');
+const linkArray = Array.from(links);
+const miObjeto = linkArray.map(link => {
+  return {href: link.href,text: link.textContent}
+});
+//const texts = linkArray.map(link => link.textContent);
+const miObjetoFinal= miObjeto.filter((elemento)=>elemento.href.startsWith('http'))
+console.log(miObjetoFinal) */
