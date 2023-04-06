06d96331cf220873e531e9d9bb04f4555321af7f
YesicaChui <yesicachuic@gmail.com>
2023-04-05 21:11:08 -0500

    cambiando el uso de readFileSync por readFile en  leerArchivo y trabajando con la promesa asincronicamente

--- src/api.js
+++ src/api.js
@@ -9,54 +9,62 @@ export const mdLinks = (path, options) => new Promise((resolve, reject) => {
   if (!path) reject("no hay path")
   if (!verificarRuta(path)) reject("ruta invalida verifique si la ruta es correcta")
   const arregloResultado = []
-  // genero el arreglo de promesas
+  // genero el arreglo de promesas http
   const promesasHttp = []
+
+  // generar el arreglo de promesas de lectura de archivos
+  const arregloPromesasLectura = []
  // obtenengo los archivos si es un directorio en un arreglo
   const arrayArchivos=obtenerArchivos(path)
-  // recorro todas las rutas del arreglo de archivos
   for(const ruta of arrayArchivos){
     if (!verificarArchivoMD(ruta)) continue
-    const contenido = leerArchivo(ruta)
-    const textoHtml = convertirTextoMDEnHtml(contenido)
-    const etiquetasA = seleccionarEtiquetasADeHtml(textoHtml)
-   // recorro cada etiqueta a
-    for (const elemento of etiquetasA) {
-      // si no inicia con http pasamos a la siguiente vuelta
-      if (!elemento.href.startsWith("http")) continue
-      // si es true validate analizo el codigo de estado(status) y el ok
-      if (options.validate) {
-        // verifico el codigo de respuesta y el ok para cada link
-        const promesa = verificarCodigoEstadoHttp(elemento.href)
-          .then(estado => {
-            //indicar fail u ok segundo codigo http          
-            arregloResultado.push({
-              href: elemento.href,
-              text: elemento.textContent,
-              file: ruta,
-              // uso el spread para desparramar el objeto estado donde estan status y ok
-              ...estado
-            })
-          })
-          // agrego al arreglo de promesas
-          promesasHttp.push(promesa)
-          // agrego al arreglo vacio el objeto con el formato requerido href, text y file
-      } else {
-        arregloResultado.push({
-          href: elemento.href,
-          text: elemento.textContent,
-          file: ruta
-        })
-      }
-    }
+    const promesaLectura = leerArchivo(ruta)
+    arregloPromesasLectura.push(promesaLectura)
   }
 
- 
-  if (options.validate) {
-    // en el caso validate true ejecuto promise all para esperar a todas las promesas terminen
-    Promise.all(promesasHttp).then(()=>resolve(arregloResultado))
-  }else{
-    resolve(arregloResultado)
-  }  
+  Promise.all(arregloPromesasLectura)
+  .then((arregloContenido)=>{
+    for(const objetoContenido of arregloContenido){
+      const textoHtml = convertirTextoMDEnHtml(objetoContenido.contenidoArchivoMD)
+      const etiquetasA = seleccionarEtiquetasADeHtml(textoHtml)
+     // recorro cada etiqueta a
+      for (const elemento of etiquetasA) {
+        // si no inicia con http pasamos a la siguiente vuelta
+        if (!elemento.href.startsWith("http")) continue
+        // si es true validate analizo el codigo de estado(status) y el ok
+        if (options.validate) {
+          // verifico el codigo de respuesta y el ok para cada link
+          const promesa = verificarCodigoEstadoHttp(elemento.href)
+            .then(estado => {
+              //indicar fail u ok segundo codigo http          
+              arregloResultado.push({
+                href: elemento.href,
+                text: elemento.textContent,
+                file: objetoContenido.ruta,
+                // uso el spread para desparramar el objeto estado donde estan status y ok
+                ...estado
+              })
+            })
+            // agrego al arreglo de promesas
+            promesasHttp.push(promesa)
+            // agrego al arreglo vacio el objeto con el formato requerido href, text y file
+        } else {
+          arregloResultado.push({
+            href: elemento.href,
+            text: elemento.textContent,
+            file: objetoContenido.ruta,
+          })
+        }
+      }
+    }
+    if (options.validate) {
+      // en el caso validate true ejecuto promise all para esperar a todas las promesas terminen
+      Promise.all(promesasHttp).then(()=>resolve(arregloResultado))
+    }else{
+      resolve(arregloResultado)
+    }  
+  })
+
 })
 
 
--- src/funciones.js
+++ src/funciones.js
@@ -7,22 +7,17 @@ import { marked } from 'marked'
 import axios from 'axios';
 import path from 'path';
 
-export const leerArchivo = (ruta) => {
-  // leo el archivo md
-  const contenidoArchivo = fs.readFileSync(ruta, 'utf-8')
-  console.log(contenidoArchivo.split(/\r?\n/)[0])
-  return contenidoArchivo
-}
-
-/* export const leerArchivo = (ruta) => new Promise((resolve, reject)=>{
-  // leo el archivo md
-  //const contenidoArchivo = fs.readFileSync(ruta, 'utf-8')
-  fs.promises.readFile(ruta,'utf-8')
-    .then((contenidoArchivo)=>resolve(contenidoArchivo))
-    .catch((error)=>reject(error))
- // console.log(contenidoArchivo.split(/\r?\n/)[1])
-  
-}) */
+// lectura asincrona de un archivo
+export const leerArchivo = (ruta) => new Promise((resolve, reject) => {
+  fs.promises.readFile(ruta, 'utf-8')
+    .then((contenidoArchivo) => resolve(
+      {
+        "contenidoArchivoMD": contenidoArchivo,
+        "ruta": ruta
+      }
+    ))
+    .catch((error) => reject(error))
+})
 
 export const convertirTextoMDEnHtml = (textoMD) => {
   // convierto texto md a html
@@ -50,47 +45,47 @@ export const verificarArchivoMD = (ruta) => {
   return extensionArchivo === 'md'
 }
 
-export const verificarCodigoEstadoHttp = (href) => new Promise((resolve,reject)=>{
+export const verificarCodigoEstadoHttp = (href) => new Promise((resolve, reject) => {
   // en cualquier caso retorno resolve pero con fail en el catch
   axios.get(href)
-  .then(response => resolve({status:response.status,ok:"ok"}))
-  // si el codigo es de 400 para arriba
-  .catch(error => {
-    // respuesta del servidor
-    if(error.response) resolve({status:error.response.status, ok:"fail"})
-    // respuesta personalizada si falla
-    else resolve({status:404, ok:"fail"})
-  })
+    .then(response => resolve({ status: response.status, ok: "ok" }))
+    // si el codigo es de 400 para arriba
+    .catch(error => {
+      // respuesta del servidor
+      if (error.response) resolve({ status: error.response.status, ok: "fail" })
+      // respuesta personalizada si falla
+      else resolve({ status: 404, ok: "fail" })
+    })
 })
 
-export const obtenerArchivos=(ruta)=>{
+export const obtenerArchivos = (ruta) => {
   // guardo los archivos en el arreglo de archivos
-  let arregloArchivos=[]
-  if(esDirectorio(ruta)){
+  let arregloArchivos = []
+  if (esDirectorio(ruta)) {
     // leo las rutas del directorio
-    const rutas =leerDirectorio(ruta)
+    const rutas = leerDirectorio(ruta)
     // a cada ruta verifico si hay más directorios de forma recursiva
-    for(const elemento of rutas){
+    for (const elemento of rutas) {
       // concateno el arreglo de archivos con el arreglo resultado de la recursividad en el directorio
-      arregloArchivos=arregloArchivos.concat(obtenerArchivos(elemento))
+      arregloArchivos = arregloArchivos.concat(obtenerArchivos(elemento))
     }
-  }else{
+  } else {
     // caso base, si no es directorio es un archivo y retorno el arreglo con un solo archivo
     arregloArchivos.push(ruta)
   }
   return arregloArchivos
 }
 
-export const esDirectorio =(ruta)=>{ //../desarrollo/laboratoria/yesica
+export const esDirectorio = (ruta) => { //../desarrollo/laboratoria/yesica
   // leo el estado de la ruta
   const stats = fs.statSync(ruta);
   // retorno true si es directorio
   return stats.isDirectory()
 }
 
-export const leerDirectorio=(ruta)=>{
+export const leerDirectorio = (ruta) => {
   // leo el directorio y le añado a cada archivo la ruta inicial
-  return fs.readdirSync(ruta).map(file =>path.join(ruta,file))
+  return fs.readdirSync(ruta).map(file => path.join(ruta, file))
 }
 //console.log(obtenerArchivos("..//node_modules"))
 
